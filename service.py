1dc12450db8410d5f036abee021274415e6d2176
Lleonsnow <159558382+Lleonsnow@users.noreply.github.com>
2024-03-27 23:02:46 +0300

    Update service.py

--- service.py
+++ service.py
@@ -2,47 +2,48 @@ from headers_params_links_errors import errors
 from typing import Set, Tuple, Optional
 from functools import reduce
 from time import sleep
+import http
 import os
 
 
 class Checker:
     def __init__(self, local, virtual, file_log, token, timer, logger):
         self.logger = logger
-        self.params = {1: local,
-                       2: virtual,
-                       3: file_log,
-                       4: token,
-                       5: timer}
+        self.params = {1: local, 2: virtual, 3: file_log, 4: token, 5: timer}
 
     def validate(self):
         if not os.path.exists(self.params[1]):
-            self.logger.error('[-] Путь к локальной директории должен быть строкой вида'
-                              ' X:\\[название папки]\\... либо не указан\\не существует')
+            self.logger.error(
+                "[-] Путь к локальной директории должен быть строкой вида"
+                " X:\\[название папки]\\... либо не указан\\не существует"
+            )
             return False
 
         else:
-            self.logger.info('[+] Путь к локальной директории корректный')
+            self.logger.info("[+] Путь к локальной директории корректный")
             sleep(0.8)
 
         if not self.params[2]:
-            self.logger.error('[-] Путь к директории на облачном хранилище должен быть строкой вида'
-                              ' directory... либо не указан')
+            self.logger.error(
+                "[-] Путь к директории на облачном хранилище должен быть строкой вида"
+                " directory... либо не указан"
+            )
             return False
 
         else:
-            self.logger.info('[+] Путь к виртуальной директории корректный')
+            self.logger.info("[+] Путь к виртуальной директории корректный")
             sleep(0.8)
 
         if not self.params[3] and not os.path.exists(self.params[3]):
-            self.logger.error('[-] Путь к файлу лога отсутствует или не существует')
+            self.logger.error("[-] Путь к файлу лога отсутствует или не существует")
             return False
 
         else:
-            self.logger.info('[+] Путь к файлу лога корректен')
+            self.logger.info("[+] Путь к файлу лога корректен")
             sleep(0.8)
 
         if not self.params[4]:
-            self.logger.error('[-] Токен приложения отсутствует')
+            self.logger.error("[-] Токен приложения отсутствует")
             return False
 
         else:
@@ -50,11 +51,13 @@ class Checker:
             sleep(0.8)
 
         if not self.params[5]:
-            self.logger.error('[-] Период для синхронизации должен быть целым числом в секундах [10] либо не указан')
+            self.logger.error(
+                "[-] Период для синхронизации должен быть целым числом в секундах [10] либо не указан"
+            )
             return False
 
         else:
-            self.logger.info('[+] Период синхронизации указан\n')
+            self.logger.info("[+] Период синхронизации указан\n")
             sleep(0.8)
 
         return True
@@ -78,24 +81,33 @@ def search_difference(new, old) -> Optional[Tuple[Set[str], Set[str], Set[str]]]
     difference_all.update(difference_old)
 
     intersection_files = new[0].symmetric_difference(old[0])
-    result = [*reduce(lambda x, y: x + (y[:1] if y[0] not in difference_all else ()), intersection_files, ())]
+    result = [
+        *reduce(
+            lambda x, y: x + (y[:1] if y[0] not in difference_all else ()),
+            intersection_files,
+            (),
+        )
+    ]
     update = set(result)
 
     return difference_new, difference_old, update
 
 
 def error_handler(logger, status_code: int) -> None:
-    if status_code == 401:
-        logger.error('[-] ' + errors[401])
+    if status_code == http.HTTPStatus.UNAUTHORIZED:
+        logger.error("[-] " + errors["UNAUTHORIZED"])
 
-    elif status_code == 409:
-        logger.error('[-] ' + errors[409])
+    elif status_code == http.HTTPStatus.CONFLICT:
+        logger.error("[-] " + errors["CONFLICT"])
 
-    elif status_code in [403, 507]:
-        logger.error('[-] ' + errors[(403, 507)])
+    elif status_code == (
+        http.HTTPStatus.FORBIDDEN,
+        http.HTTPStatus.INSUFFICIENT_STORAGE,
+    ):
+        logger.error("[-] " + errors["FORBIDDEN"])
 
-    elif status_code == 413:
-        logger.error('[-] ' + errors[413])
+    elif status_code == http.HTTPStatus.REQUEST_ENTITY_TOO_LARGE:
+        logger.error("[-] " + errors["REQUEST_ENTITY_TOO_LARGE"])
 
-    elif status_code in [423, 503]:
-        logger.error('[-] ' + errors[423, 503])
+    elif status_code in (http.HTTPStatus.SERVICE_UNAVAILABLE, http.HTTPStatus.LOCKED):
+        logger.error("[-] " + errors["LOCKED"])
