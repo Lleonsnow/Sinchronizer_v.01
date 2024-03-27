e6040a10d98a5d5df42fad83c6cd9a77972b1dd1
Lleonsnow <159558382+Lleonsnow@users.noreply.github.com>
2024-03-27 23:04:45 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 
 class SiteSettings(BaseSettings):
-    TOKEN: SecretStr = os.getenv('TOKEN', None)
-    local_path: SecretStr = os.getenv('LOCAL_PATH', None)
-    virtual_path: SecretStr = os.getenv('VIRTUAL_PATH', None)
-    period: SecretStr = os.getenv('SINCHRONIZATION_PERIOD', None)
-    log_path: SecretStr = os.getenv('LOG_PATH', None)
+    TOKEN: SecretStr = os.getenv("TOKEN", None)
+    local_path: SecretStr = os.getenv("LOCAL_PATH", None)
+    virtual_path: SecretStr = os.getenv("VIRTUAL_PATH", None)
+    period: SecretStr = os.getenv("SINCHRONIZATION_PERIOD", None)
+    log_path: SecretStr = os.getenv("LOG_PATH", None)
